5401f273624ed64af8a62f8400c78d61dc0cf472
abigailwright89 <102739142+abigailwright89@users.noreply.github.com>
2024-01-25 11:12:15 +0000

    adds more indepth search results

--- productFinderApp/src/components/AnalysisResults.jsx
+++ productFinderApp/src/components/AnalysisResults.jsx
@@ -1,29 +1,34 @@
-function AnalysisResults ({analysisResults}) {
-   return( 
-    <>
-    {analysisResults && (
-    
-<div>
-      <h2>Analysis Results:</h2>
-      <ul>
-        {analysisResults.webEntities.map((entity, index) => (
-          <li key={index}>{entity.description}</li>
-        ))}
-      </ul>
+function AnalysisResults({ analysisResults }) {
+  return (
+    <div>
+      {analysisResults && (
+        <>
+          {analysisResults.pagesWithMatchingImages && (
+            <>
+              <h2>Pages with Matching Images:</h2>
+              <ul>
+                {analysisResults.pagesWithMatchingImages.map((page, index) => (
+                  <li key={index}>
+                    URL: <a href={page.url} target="_blank" rel="noopener noreferrer">{page.url}</a>
+                  </li>
+                ))}
+              </ul>
+            </>
+          )}
 
-      <h2>Matching Images:</h2>
-      <ul>
-        {analysisResults.visuallySimilarImages.map((image, index) => (
-          <li key={index}>
-            <img src={image.url} alt={`Matching Image ${index + 1}`} />
-            <p>URL: <a href={image.url} target="_blank" rel="noopener noreferrer">{image.url}</a></p>
-          </li>
-        ))}
-      </ul>
+          <h2>Visually Similar Images:</h2>
+          <ul>
+            {analysisResults.visuallySimilarImages && analysisResults.visuallySimilarImages.map((image, index) => (
+              <li key={index}>
+                <img src={image.url} alt={`Matching Image ${index + 1}`} />
+                <p>URL: <a href={image.url} target="_blank" rel="noopener noreferrer">{image.url}</a></p>
+              </li>
+            ))}
+          </ul>
+        </>
+      )}
     </div>
-    
-  )}
-  </>
-)};
+  );
+}
 
 export default AnalysisResults;
